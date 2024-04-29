3a1477a78f4df74a23450ab3d1f4e315ae5a47be
Dmitry <dima.shevchenko10@gmail.com>
2024-04-29 13:30:30 +0300

    styles for header and menuItem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import i18n from "../i18n";
 import { locales } from "../types/locales";
 import { MenuItem } from "@mui/material";
+import style from "../scss/locale.module.scss";
+import logo from "../img/logo.jpg";
 
 const Header = () => {
 
   return (
-    <header>
-      <img alt="some test text" />
+    <header className={style.header}>
+      <img src={logo} />
       <div>
           {Object.keys(locales).map((locale) => 
-              <MenuItem key = {locale}
-                onClick={()=>i18n.changeLanguage(locale)}>
+              <MenuItem 
+                className={style.menuItem} 
+                key = {locale}
+                onClick={()=>i18n.changeLanguage(locale)}
+                >
                   {locales[locale as keyof typeof locales]}
               </MenuItem>
           )}
