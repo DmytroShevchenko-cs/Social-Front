b1abb95f436a53aecf4a9c66b434c53cc95ef85a
Dmitry <dima.shevchenko10@gmail.com>
2024-05-01 18:45:51 +0300

    beautify

--- src/components/LocaleButtons.tsx
+++ src/components/LocaleButtons.tsx
@@ -7,16 +7,16 @@ const localeButtons = () => {
 
     return(
         <div className = {style.button_container}>
-        {Object.keys(locales).map((locale) => 
-            <button 
-              className={style.menuItem} 
-              key = {locale}
-              onClick={()=>i18n.changeLanguage(locale)}
-              >
-                {locales[locale as keyof typeof locales]}
-            </button>
-        )}
-    </div>
+            {Object.keys(locales).map((locale) => 
+                <button 
+                className={style.menuItem} 
+                key = {locale}
+                onClick={()=>i18n.changeLanguage(locale)}
+                >
+                    {locales[locale as keyof typeof locales]}
+                </button>
+            )}
+        </div>
     );
 };
 
