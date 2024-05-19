7ba19970233b835ea3b3b659ef19f9ea199e8422
Dmitry <dima.shevchenko10@gmail.com>
2024-05-19 03:26:34 +0300

    navigate buttons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../img/logo.png";
+import logo from "../../img/header/logo.png";
 import LocaleButtons from "./LocaleButtons"
 import NavigateButtons from "./NavigateButtons";
 
--- src/components/header/NavigateButtons.tsx
+++ src/components/header/NavigateButtons.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import styles from "../../scss/navigate.module.scss"
 
 const NavigateButtons = () => {
 
     return (
-       <div className="navigate">
-            
+       <div className={styles.navigate}>
+            <div className={styles.navigateItem}>
+                <button className={styles.home}></button>
+                <label>Home</label>
+            </div>
+            <div className={styles.navigateItem}>
+            <button className={styles.friends}></button>
+                <label>Friends</label>
+            </div>
+            <div className={styles.navigateItem}>
+            <button className={styles.groups}></button>
+                <label>Groups</label>
+            </div>
+            <div className={styles.navigateItem}>
+            <button className={styles.chats}></button>
+                <label>Chats</label>
+            </div>
+            <div className={styles.navigateItem}>
+                <button className={styles.notifications}></button>
+                <label>Notifications</label>
+            </div>               
        </div>
     );
 };
