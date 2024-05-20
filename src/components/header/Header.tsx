028281c768ef53dd3eb65dd253ab1fdae63d5bde
Dmitry <dima.shevchenko10@gmail.com>
2024-05-20 21:59:07 +0300

    locale

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import logo from "../../img/header/logo.png";
+import logo from "../../img/header/logo.svg";
 import LocaleButtons from "./LocaleButtons"
 import NavigateButtons from "./NavigateButtons";
+import styles from "../../scss/layout.module.scss"
+import Search from "./Search";
 
-const Header = () => {
-
+const Header = () => {  
   return (
     <header>
-      <img src={logo} alt="Logo"/>
-      <NavigateButtons/>
+      <div className={styles.headerContent}>
+        <img src={logo} alt="Logo"/>
+        <Search/>
+        <NavigateButtons/>
+      </div>
       <LocaleButtons />
     </header>
   );
--- src/components/header/NavigateButtons.tsx
+++ src/components/header/NavigateButtons.tsx
@@ -1,30 +1,44 @@
 import React from "react";
 import styles from "../../scss/navigate.module.scss"
+import { useTranslation } from 'react-i18next';
+import '../../i18n';
 
 const NavigateButtons = () => {
+    const {t} = useTranslation();
 
+    const homeLabel = t('header.home');
+    const friendsLabel = t('header.friends');
+    const groupsLabel = t('header.groups');
+    const chatsLabel = t('header.chats');
+    const notificationsLabel = t('header.notifications');
+    const meLabel = t('header.me');
+    
     return (
        <div className={styles.navigate}>
             <div className={styles.navigateItem}>
                 <button className={styles.home}></button>
-                <p>Home</p>
+                <p>{homeLabel}</p>
             </div>
             <div className={styles.navigateItem}>
             <button className={styles.friends}></button>
-                <p>Friends</p>
+                <p>{friendsLabel}</p>
             </div>
             <div className={styles.navigateItem}>
             <button className={styles.groups}></button>
-                <p>Groups</p>
+                <p>{groupsLabel}</p>
             </div>
             <div className={styles.navigateItem}>
             <button className={styles.chats}></button>
-                <p>Chats</p>
+                <p>{chatsLabel}</p>
             </div>
             <div className={styles.navigateItem}>
                 <button className={styles.notifications}></button>
-                <p>Notifications</p>
-            </div>               
+                <p>{notificationsLabel}</p>
+            </div>
+            <div className={styles.navigateItem}>
+                <button className={styles.me}></button>
+                <p>{meLabel}</p>
+            </div>
        </div>
     );
 };
